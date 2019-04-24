22297f0a3be27634998340d32527577869aa0892
kulikov98 <kulikov98@gmail.com>
2019-04-24 22:05:23 +0300

    autoload

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require __DIR__ . '/../vendor/autoload.php';
+require('../vendor/autoload.php');
 
 session_start();
 
